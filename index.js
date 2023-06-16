750bcb357582db6d2545dec4c2dec3faa6516d2f
Samuel <samuelou510@gmail.com>
2023-06-16 01:28:38 -0400

    send data with fetch POST lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+function submitData(name, email) {
+  const nameEmail = {
+    name: name,
+    email: email,
+  };
+
+  const configObject = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(nameEmail),
+  };
+
+  return fetch(`http://localhost:3000/users`, configObject)
+    .then((res) => res.json())
+    .then((data) => {
+      const body = document.querySelector("body");
+      body.append(data.id);
+    })
+    .catch((error) => {
+      const body = document.querySelector("body");
+      body.append(`${error} message sam`);
+    });
+}
+//1) HOW TO CLEAR MY GOD DAMN USERS JSON SERVER
+//2) IS IT APPENDING CORRECTLY, IT JUST DISAPPEARS AND FOR SOME REASON
+// IT REFRESHES
+// submitData("sam", "sam@gmail.com");
